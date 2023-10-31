2b1567a89c2462431dac89faa4e333f6d9c4b10a
Gagan Deep Kashyap <gagandeepk.610weblab@gmail.com>
2023-10-31 10:04:44 +0530

    ROOT Added

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('root');
+});
 
 Auth::routes();
 
-Route::get('/', 'HomeController@redirectAdmin')->name('index');
+// Route::get('/', 'HomeController@redirectAdmin')->name('index');
 Route::get('/home', 'HomeController@index')->name('home');
 
 /**
